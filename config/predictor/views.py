bfa7dbc7f76667ccfe4b49d96656bda37ccac0fa
akfoz45 <akf.zdemir@gmail.com>
2026-05-17 09:50:55 +0300

    Feature Importance added

--- config/predictor/views.py
+++ config/predictor/views.py
@@ -6,7 +6,8 @@ from django.shortcuts import render
 from .forms import HousePriceForm
 
 def predict_price_view(request):
-    prediction = None
+    prediction_formatted = None    
+    ai_comment = None
 
     if request.method == 'POST':
         form = HousePriceForm(request.POST)
@@ -21,8 +22,26 @@ def predict_price_view(request):
             if model:
                 pred_log = model.predict(df)[0]
                 pred_value = np.expm1(pred_log)
-                prediction = round(pred_value, 2)
+
+                prediction_formatted = "{:,.0f}".format(pred_value).replace(',', '.')
+
+                district = input_data.get("district_name")
+                m2 = input_data.get("net_sqm")
+                room = input_data.get("room_count")
+
+                ai_comment = (
+                    f"Modelin analizine göre; evin {district} ilçesinde bulunması ve "
+                    f"{m2} m² net kullanım alanına sahip olması bu değerlemenin "
+                    f"belirlenmesindeki en büyük etkenler olmuştur."
+                )
+
     else:
         form = HousePriceForm()
 
-    return render(request, 'predictor/index.html', {'form': form, 'prediction' : prediction})
+    context = {
+        'form' : form,
+        'prediction' : prediction_formatted,
+        'ai_comment' : ai_comment
+    }
+
+    return render(request, 'predictor/index.html', context)
