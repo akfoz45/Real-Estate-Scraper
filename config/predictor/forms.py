def2e0434d3256cc78d0239ea26700aae6500388
akfoz45 <akf.zdemir@gmail.com>
2026-05-17 10:20:59 +0300

    Interface fixed.

--- config/predictor/forms.py
+++ config/predictor/forms.py
@@ -1,23 +1,24 @@
 from django import forms
 
 class HousePriceForm(forms.Form):
-    district_name = forms.CharField(label='İlçe (Örn: Buca)', max_length=100)
-    neighborhood_name = forms.CharField(label='Mahalle (Örn: Adatepe)', max_length=100)
-    gross_sqm = forms.FloatField(label='Brüt Metrekare')
-    net_sqm = forms.FloatField(label='Net Metrekare') 
-    building_age_num = forms.IntegerField(label='Bina Yaşı (Sıfır ise 0)')
-    total_rooms = forms.IntegerField(label='Toplam Oda Sayısı (Örn: 3+1 için 4)')
-    floor_level_num = forms.IntegerField(label='Bulunduğu Kat (-1, 1, 4 vb.)')
-
-    IS_FURNISHED_CHOICES = [
-        ('Eşyalı', 'Eşyalı'),
-        ('Eşyalı Değil', 'Eşyalı Değil')
-    ]
-    is_furnished = forms.ChoiceField(label="Eşya Durumu", choices=IS_FURNISHED_CHOICES)
-
+    district_name = forms.CharField(label="İlçe", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Örn: Kadıköy'}))
+    neighborhood_name = forms.CharField(label="Mahalle", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Örn: Moda'}))
+    net_sqm = forms.FloatField(label="Net Metrekare", widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Örn: 120'}))
+    gross_sqm = forms.FloatField(label="Brüt Metrekare", widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Örn: 140'}))
+    total_rooms = forms.IntegerField(label="Oda Sayısı", widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Örn: 4'}))
+    building_age_num = forms.IntegerField(label="Bina Yaşı", widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Örn: 5'}))
+    floor_level_num = forms.IntegerField(label="Bulunduğu Kat", widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Örn: 3'}))
+    
     HEATING_CHOICES = [
-        ('Kombi', 'Kombi'),
-        ('Merkezi', 'Merkezi'),
+        ('Kombi', 'Kombi'), 
+        ('Merkezi', 'Merkezi'), 
+        ('Yerden Isıtma', 'Yerden Isıtma'), 
         ('Diğer', 'Diğer')
     ]
-    heating_type = forms.ChoiceField(label='Isıtma Tipi', choices=HEATING_CHOICES)
+    heating_type = forms.ChoiceField(choices=HEATING_CHOICES, widget=forms.Select(attrs={'class': 'form-select'}))
+    
+    FURNISHED_CHOICES = [
+        ('Eşyalı', 'Eşyalı'), 
+        ('Eşyalı Değil', 'Eşyalı Değil')
+    ]
+    is_furnished = forms.ChoiceField(choices=FURNISHED_CHOICES, widget=forms.Select(attrs={'class': 'form-select'}))
